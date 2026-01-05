ce389755fefe6e2498ae5457e6b637d10a728072
Thomas Rambø <thms.rmb@gmail.com>
2026-01-05 14:28:02 +0100

    wip

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t validator . && docker run -it --rm -p 8080:8080 --name validator validator:latest
+docker build -t validator . && docker run -it --rm -p 8000:8000 --name validator validator:latest
